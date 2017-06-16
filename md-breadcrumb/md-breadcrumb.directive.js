8d6afa76a2150dede01d060991aee813ef5170d8
Ram-SR <rammurthythati@gmail.com>
2017-06-16 16:08:51 +0530

    few changes

--- md-breadcrumb/md-breadcrumb.directive.js
+++ md-breadcrumb/md-breadcrumb.directive.js
@@ -1,7 +1,14 @@
+/*
+ * Author: rammurthy
+ * version: v1.0
+ * reference: https://ui-router.github.io/ng1/
+*/
+
+// breadcrumb directive definition
 function breadcrumbDirective() {
     return {
         restrict: 'EA',
-        templateUrl: 'app/common/directives/md-breadcrumb/breadcrumb/breadcrumb.tmpl.html',
+        templateUrl: './md-breadcrumb/breadcrumb/breadcrumb.tmpl.html',
         controller: breadcrumbController
     }
 }
